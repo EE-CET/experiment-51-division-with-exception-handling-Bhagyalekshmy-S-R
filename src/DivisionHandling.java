d074439955993293082c6e35db69eb98f053a154
Your Name <you@example.com>
2026-02-13 09:23:48 +0530

    first commit

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
 public class DivisionHandling {
-    
-        // TODO: Read two integers a and b
-        
-        // TODO: Enclose the division in a try block
-        
-        // TODO: Catch ArithmeticException
+        public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        int result=0;
+        try{
+            result=a/b;
+            System.out.println(result);
+           }
+         catch (ArithmeticException e){
+             System.out.println("Division by zero error");
+             }
+         sc.close();
+       
+         }    
         
 }
