d3fac7343db82b8c40b23688f43d9454c1e8bc03
Your Name <you@example.com>
2026-02-13 09:26:55 +0530

    first commit

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -11,7 +11,7 @@ public class DivisionHandling {
             System.out.println(result);
            }
          catch (ArithmeticException e){
-             System.out.println("Division by zero error");
+             System.out.println("Divide by zero error");
              }
          sc.close();
        
